df20d83b4f385535d24b9d2f85f8899cff7aad1f
este36 <mercierstban@gmail.com>
2025-11-18 17:11:09 +0100

    fix ft_lstpop segfault

--- 42-Libft/ft_lstpop_front.c
+++ 42-Libft/ft_lstpop_front.c
@@ -6,7 +6,7 @@
 /*   By: emercier <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/11/18 13:25:13 by emercier       #+#    #+#                */
-/*   Updated: 2025/11/18 16:51:40 by emercier       ########   odam.nl        */
+/*   Updated: 2025/11/18 17:10:40 by emercier       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_list	*ft_lstpop_front(t_list **lst)
 {
 	t_list	*prev_head;
 
+	if (*lst == NULL)
+		return (NULL);
 	prev_head = *lst;
 	*lst = (*lst)->next;
 	return (prev_head);
