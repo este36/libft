c07e14bcc82f5cd2325bfbf0e1c9fd2400185cc9
este36 <mercierstban@gmail.com>
2025-11-18 20:27:57 +0100

    fix ft_realloc

--- 42-Libft/ft_realloc.c
+++ 42-Libft/ft_realloc.c
@@ -6,9 +6,11 @@
 /*   By: emercier <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/11/18 19:10:45 by emercier       #+#    #+#                */
-/*   Updated: 2025/11/18 19:21:49 by emercier       ########   odam.nl        */
+/*   Updated: 2025/11/18 20:27:43 by emercier       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_realloc(void *ptr, size_t prev_size, size_t size)
 {
@@ -17,6 +19,6 @@ void	*ft_realloc(void *ptr, size_t prev_size, size_t size)
 	new = malloc(size);
 	if (!new)
 		return (NULL);
-	memcpy(new, ptr, prev_size);
+	ft_memcpy(new, ptr, prev_size);
 	return (new);
 }
