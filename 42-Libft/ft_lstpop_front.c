fa55bcaa3753ebbc15395ad9bb8e15feab4a7ff8
este36 <mercierstban@gmail.com>
2025-11-19 14:50:46 +0100

    fix lst_pop

--- 42-Libft/ft_lstpop_front.c
+++ 42-Libft/ft_lstpop_front.c
@@ -6,7 +6,7 @@
 /*   By: emercier <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/11/18 13:25:13 by emercier       #+#    #+#                */
-/*   Updated: 2025/11/19 14:45:40 by emercier       ########   odam.nl        */
+/*   Updated: 2025/11/19 14:50:34 by emercier       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ t_list	*ft_lstpop_front(t_list **lst)
 		return (NULL);
 	prev_head = *lst;
 	*lst = (*lst)->next;
+	prev_head->next = NULL;
 	return (prev_head);
 }
