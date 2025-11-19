4ad75b84aa3fdb334452b1d727af73bf35ca76a3
este36 <mercierstban@gmail.com>
2025-11-19 14:46:09 +0100

    fix lstaddfront

--- 42-Libft/ft_lstadd_front.c
+++ 42-Libft/ft_lstadd_front.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                   :+:    :+:           */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emercier <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:29:37 by emercier          #+#    #+#             */
-/*   Updated: 2025/10/02 15:29:47 by emercier         ###   ####lausanne.ch   */
+/*   Updated: 2025/11/19 14:44:39 by emercier       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (new == NULL)
+		return ;
 	new->next = (*lst);
 	(*lst) = new;
 }
