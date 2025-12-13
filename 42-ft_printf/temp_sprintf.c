262098be37855fec1779c2804b46e6358d3e536d
este36 <mercierstban@gmail.com>
2025-12-13 21:59:11 +0100

    temp_sprintf: add 4 static buffer

--- 42-ft_printf/temp_sprintf.c
+++ 42-ft_printf/temp_sprintf.c
@@ -6,7 +6,7 @@
 /*   By: emercier <emercier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 19:02:24 by emercier          #+#    #+#             */
-/*   Updated: 2025/12/12 19:10:39 by emercier         ###   ########.fr       */
+/*   Updated: 2025/12/13 21:53:25 by emercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 char	*temp_sprintf(const char *fmt, ...)
 {
-	static char	buf[1024];
+	static int	counter = 0;
+	static char	buf[4][512];
 	va_list		arg_list;
 	t_dstr		dest;
 
 	ft_bzero(&dest, sizeof(t_dstr));
-	dest.cap = 1024;
-	dest.buf = buf;
+	dest.cap = 512;
+	dest.buf = buf[counter % 4];
 	va_start(arg_list, fmt);
 	ft_printf_fn(write_str, &dest, fmt, &arg_list);
 	va_end(arg_list);
-	return (buf);
+	return (buf[counter++]);
 }
