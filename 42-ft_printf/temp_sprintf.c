6a5e9f3c7985c3280a499a2b48938402c84f349e
este36 <mercierstban@gmail.com>
2025-12-17 01:19:27 +0100

    fix temp_snprintf

--- 42-ft_printf/temp_sprintf.c
+++ 42-ft_printf/temp_sprintf.c
@@ -6,7 +6,7 @@
 /*   By: emercier <emercier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 19:02:24 by emercier          #+#    #+#             */
-/*   Updated: 2025/12/13 21:53:25 by emercier         ###   ########.fr       */
+/*   Updated: 2025/12/17 01:06:55 by emercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@ char	*temp_sprintf(const char *fmt, ...)
 	static char	buf[4][512];
 	va_list		arg_list;
 	t_dstr		dest;
+	int			buf_index;
 
 	ft_bzero(&dest, sizeof(t_dstr));
 	dest.cap = 512;
-	dest.buf = buf[counter % 4];
+	buf_index = counter++ % 4;
+	dest.buf = buf[buf_index];
+	ft_bzero(dest.buf, 512);
 	va_start(arg_list, fmt);
 	ft_printf_fn(write_str, &dest, fmt, &arg_list);
 	va_end(arg_list);
-	return (buf[counter++]);
+	return (buf[buf_index]);
 }
